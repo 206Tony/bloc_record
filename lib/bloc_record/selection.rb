d8e84d00e5bc2e3dae77e24d49ec8909ce2f2099
Anthony Zardis <anthonyzardis@gmail.com>
2017-12-18 13:10:34 -0800

    find_by implemented

--- lib/bloc_record/selection.rb
+++ lib/bloc_record/selection.rb
@@ -2,34 +2,62 @@ require 'sqlite3'
 
 module Selection 
 	def find(*ids)
-		if ids.length == 1
-			find_one(ids.first)
-		else
-			rows = connection.execute <<-SQL
-				SELECT #{columns.join ","} FROM #{table}
-				WHERE id IN (#{ids.join(",")});
-			SQL
+		if ids.kind_of? Integer && ids >0
+			if ids.length == 1
+				find_one(ids.first)
+			else
+				rows = connection.execute <<-SQL
+					SELECT #{columns.join ","} FROM #{table}
+					WHERE id IN (#{ids.join(",")});
+				SQL
 
-			rows_to_array(rows)
-		end
+				rows_to_array(rows)
+			end
+		else
+			puts "Invalid id. Please enter a valid id."
 	end
 
 	def find_one(id)
-		row = connection.get_first_row <<-SQL 
-			SELECT #{columns.join ","} FROM #{table}
-			WHERE id = #{id};
-		SQL
+		if ids.kind_of? Interger && ids > 0
+			row = connection.get_first_row <<-SQL 
+				SELECT #{columns.join ","} FROM #{table}
+				WHERE id = #{id};
+			SQL
 
-		init_object_from_row(row)
+			init_object_from_row(row)
+		else
+			puts "Invalid id. Please enter a valid id."
+		end
 	end
 
 	def find_by(attribute, value)
 		row = connection.get_first_row <<-SQL
-			SELECT #{columns.join ","} FROM #{table}
-			WHERE #{attribute} = {BlocRecord::Utility.sql_strings(value)};
+			SELECT #{attribute} FROM #{table}
+			WHERE #{value} = {BlocRecord::Utility.sql_strings(value)};
 		SQL
 
 		init_object_from_row(row)
+	end
+
+	def find_each(options={})
+		row = connection.execute <<-SQL
+			SELECT * FROM #{table}
+			ORDER BY #{table}
+			LIMIT #{options[:batch_size]};
+		SQL
+
+		for row in rows_to_array(rows)
+			yield(row)
+		end
+	end
+
+	def find_in_batches(start, batch_size)
+		rows = connection.execute <<-SQL
+			SELECT #{colums.join ","} FROM #{table}
+			LIMIT #{start}, #{batch_size};
+		SQL
+
+		yield(rows_to_array(rows))
 	end
 
 	def take(num=1)
@@ -83,6 +111,7 @@ module Selection
 	end
 
 	private 
+
 	def init_object_from_row(row)
 		if row 
 			data = Hash[columns.zip(row)]
@@ -92,5 +121,15 @@ module Selection
 
 	def rows_to_array(rows)
 		rows.map { |row| new(Hash[columns.zip(row)]) }
+	end
+
+	def method_missing(methId, *args)
+		attribute = methId.to_s
+		if columns.include?(attribute)
+			find_by(attribute, *args)
+		else
+			puts "No #{attribute} found.  Please try again."
+		end
+		method_missing(methId, *args)
 	end
 end
