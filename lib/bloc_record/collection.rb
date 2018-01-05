0b78b312ed5fd4b2e281fb658f5f4a820a927730
Anthony Zardis <anthonyzardis@gmail.com>
2018-01-04 20:14:29 -0800

    assign-destroy

--- lib/bloc_record/collection.rb
+++ lib/bloc_record/collection.rb
@@ -1,8 +1,15 @@
 module BlocRecord
 	class Collection < Array 
+		
 		def update_all(updates)
-			ids = self.map(&:id)
+			ids = self.map(&id)
 			self.any? ? self.first.class.update(ids, updates) : false
+		end
+
+		def destroy_all
+			self.each do |item|
+				self.forst.class.destroy(item.id)
+			end
 		end
 	end
 end
--- lib/bloc_record/persistence.rb
+++ lib/bloc_record/persistence.rb
@@ -2,6 +2,7 @@ require 'sqlite3'
 require 'bloc_record/schema'
 
 module Persistence
+	
 	def self.included(base)
 		base.extend(ClassMethods)
 	end
@@ -24,11 +25,12 @@ module Persistence
 			SET #{fields}
 			WHERE id = #{self.id};
 		SQL
+
 		true
 	end
 
 	def update_attribute(attribute, value)
-		self.class.update(self.id, { attribute => value })
+		self.class.update(self.id, { attribute => value } )
 	end
 
 	def update_attributes(updates)
@@ -40,6 +42,7 @@ module Persistence
 	end
 
 	module ClassMethods
+
 		def create(attrs)
 			attrs = BlocRecord::Utility.convert_keys(attrs)
 			attrs.delete "id"
@@ -56,22 +59,32 @@ module Persistence
 		end
 
 		def update(ids, updates)
-			updates = BlocRecord::Utility.convert_keys(updates)
-			updates.delete "id"
-			updates_array = updates.map { |key, value| "{key}=#{BlocRecord::Utility.sql_strings(value)}" }
-			if ids.class == Fixnum
-				where_clause = "WHERE id = #{ids};"
-			elsif ids.class == Array 
-				where_clause = ids.empty? ? ";" : "WHERE id IN (#{ids.join(",")});"
-			else
-				where_clause = ";"
-			end
+			case updates
+			when Hash
+				updates = BlocRecord::Utility.convert_keys(updates)
+				updates.delete "id"
+				
+				updates_array = updates.map { |key, value| "{key}=#{BlocRecord::Utility.sql_strings(value)}" }
+				
+				if ids.class == Fixnum
+					where_clause = "WHERE id = #{ids};"
+				elsif ids.class == Array 
+					where_clause = ids.empty? ? ";" : "WHERE id IN (#{ids.join(",")});"
+				else
+					where_clause = ";"
+				end
 
-			connection.execute <<-SQL
-				UPDATE #{table}
-				SET #{updates_array * ","} #{where_clause}
-			SQL
-			true
+				connection.execute <<-SQL
+					UPDATE #{table}
+					SET #{updates_array * ","} #{where_clause}
+				SQL
+
+				true
+			when Array 
+				updates.each_with_index do |data, index|
+					update(ids[index], data)
+				end
+			end
 		end
 
 		def update_all(updates)
@@ -94,8 +107,17 @@ module Persistence
 
 		def destroy_all(conditions_hash=nil)
 			if conditions_hash && !conditions_hash.empty?
-				conditions_hash = BlocRecord::Utility.convert_keys(conditions_hash)
-				conditions = conditions_hash.map {|key, value| "#{BlocRecord::Utility.sql_strings(value)}"}.join(" and ")
+				case conditions_hash
+				when Hash 
+					conditions_hash = BlocRecord::Utility.convert_keys(conditions_hash)
+					conditions = conditions_hash.map {|key, value| "#{BlocRecord::Utility.sql_strings(value)}"}.join(" and ")
+
+				when String 
+					conditions = conditions_hash
+
+				when Array
+					conditions = conditions_hash.join(",")
+				end
 
 				connection.execute <<-SQL
 					DELETE FROM #{table}
@@ -108,6 +130,14 @@ module Persistence
 			end
 			
 			true
+		end
+
+		def method_missing(method, *args, &block)
+			if method.include? "update"
+				meth = method.select { |key, value| arg.include(key) }
+				self.delete_if { |key, value| arg.include(key) }
+			end
+			update(m,  arg[0])
 		end
 	end
 end
